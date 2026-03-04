e6c24ab9232c1b994461a297c7da165d8e2ded2e
changhyeon <ckdgus988@gmail.com>
2026-03-04 11:59:37 +0900

    fix: global 관련 된 에러코드 및 response, handler 처리 수정

--- src/main/java/com/dailytable/dailytable/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/dailytable/dailytable/global/handler/GlobalExceptionHandler.java
@@ -1,8 +1,8 @@
 package com.dailytable.dailytable.global.handler;
 
+import com.dailytable.dailytable.global.common.ErrorCode;
 import com.dailytable.dailytable.global.exception.BaseException;
 import com.dailytable.dailytable.global.response.ApiResponse;
-import org.apache.ibatis.builder.BuilderException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,18 +11,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(BuilderException.class)
+    @ExceptionHandler(BaseException.class)
     public ResponseEntity<ApiResponse<?>> handleBusiness(BaseException e){
 
         return ResponseEntity
                 .status(e.getErrorCode().getStatus())
-                .body(ApiResponse.fail(e.getMessage()));
+                .body(ApiResponse.fail(e.getErrorCode()));
     }
-
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<?>> handleUnknown(Exception e){
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ApiResponse.fail("SYSTEM ERROR"));
+                .body(ApiResponse.fail(ErrorCode.SERVER_ERROR));
+        }
     }
-}
--- src/main/java/com/dailytable/dailytable/global/response/ApiResponse.java
+++ src/main/java/com/dailytable/dailytable/global/response/ApiResponse.java
@@ -1,5 +1,6 @@
 package com.dailytable.dailytable.global.response;
 
+import com.dailytable.dailytable.global.common.ErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,16 +11,22 @@ public class ApiResponse<T> {
     private boolean success;
     private String message;
     private T data;
+    private String errorCode;
 
     public static <T> ApiResponse<T> success(T data){
-        return new ApiResponse<>(true, "OK", data);
+        return new ApiResponse<>(true, "OK", data, null);
     }
 
     public static <T> ApiResponse<T> success(String message, T data){
-        return new ApiResponse<>(true, message, data);
+        return new ApiResponse<>(true, message, data, null);
     }
 
-    public static ApiResponse<?> fail(String message){
-        return new ApiResponse<>(false, message, null);
+    public static ApiResponse<?> fail(ErrorCode errorCode){
+        return new ApiResponse<>(
+                false,
+                errorCode.getMessage(),
+                null,
+                errorCode.name()
+        );
     }
 }
