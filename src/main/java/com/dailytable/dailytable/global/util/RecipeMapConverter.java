ed4b31242b97e5c8f2dfe6e3bc62709ebd21772d
changhyeon <ckdgus988@gmail.com>
2026-03-06 12:34:12 +0900

    fix: view, ai result 日本語で

--- src/main/java/com/dailytable/dailytable/global/util/RecipeMapConverter.java
+++ src/main/java/com/dailytable/dailytable/global/util/RecipeMapConverter.java
@@ -4,24 +4,24 @@ import java.util.Map;
 
 public class RecipeMapConverter {
     public static final Map<String, Integer> PURPOSE_MAP = Map.of(
-            "속세의맛", 1, "다이어트", 2, "건강식", 3, "술안주", 4
+            "ガッツリ系", 1, "ダイエット", 2, "ヘルシー", 3, "おつまみ", 4
     );
     public  static final Map<String, Integer> CUISINE_MAP = Map.of(
-            "상관없음", 1, "한식", 2, "일식", 3, "중식", 4, "양식", 5, "동남아", 6
+            "なんでも ", 1, "日本料理", 2, "韓国料理", 3, "中華料理", 4, "洋食", 5, "東南アジア料理", 6
     );
-    public  static final Map<String, Integer> DIFFICULTY_MAP = Map.of(
-            "상관없음", 0, "하", 1, "중", 2, "상", 3
-    );
+//    public  static final Map<String, Integer> DIFFICULTY_MAP = Map.of(
+//            "なんでも", 0, "低", 1, "中", 2, "高", 3
+//    );
     public  static final Map<String, String> DIFFICULTY_AI_MAP = Map.of(
-            "하", "LOW", "중", "MEDIUM", "상", "HIGH", "상관없음", "ANY"
+            "低", "LOW", "中", "MEDIUM", "高", "HIGH", "なんでも", "ANY"
     );
     public  static final Map<String, Integer> DIFFICULTY_RESULT_MAP = Map.of(
             "LOW", 1, "MEDIUM", 2, "HIGH", 3
     );
     public  static final Map<String, String> DIFFICULTY_LABEL_MAP = Map.of(
-            "LOW", "하", "MEDIUM", "중", "HIGH", "상"
+            "LOW", "低", "MEDIUM", "中", "HIGH", "高"
     );
     public  static final Map<Integer, String> CUISINE_STYLE_MAP = Map.of(
-            1, "Any", 2, "Korean", 3, "Japanese", 4, "Chinese", 5, "Western", 6, "Southeast Asian"
+            1, "Any", 2, "韓国料理", 3, "日本料理", 4, "中華料理", 5, "洋食", 6, "東南アジア料理"
     );
 }
